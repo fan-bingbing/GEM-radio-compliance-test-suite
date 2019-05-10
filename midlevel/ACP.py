788501d0ec89dc7d952e0a197d7b1ccdcb27d545
Aaron Fan <afan@gme.net.au>
2019-05-10 15:54:18 +1000

    add user input test frequency

--- midlevel/ACP.py
+++ midlevel/ACP.py
@@ -1,5 +1,6 @@
 import visa
 from openpyxl import load_workbook
+
 import time
 import re
 
@@ -7,18 +8,24 @@ rm = visa.ResourceManager()
 FSV = rm.open_resource('TCPIP0::192.168.10.9::hislip0::INSTR') # Spec An
 
 
-def Tx_Adjacent_channel_power():
-    FSV_file_write = load_workbook(filename = "Test_Setup.xlsx") # create a workbook from existing .xlsx file
-    sheet = FSV_file_write["ACP"] # load setup sheet in .xlsx to sheet
+def Tx_Adjacent_channel_power(Test_frequency):
 
-    Centre_frequency = sheet["B1"].value # get start frequency value from sheet
-    Span_frequency = sheet["B2"].value # get stop frequency value from sheet
-    RBW = sheet["B3"].value # get RBW value from sheet
-    VBW = sheet["B4"].value # get VBW value from sheet
-    RF_level = sheet["B5"].value # get RF_level
-    Attenuation = sheet["B6"].value # get attenuation
-    RefLev_offset = sheet["B7"].value# get RFlevel offset
-    Trace_RMS = sheet["B8"].value # get trace to Pos peak
+# below codes are for setting test frequency in Test_Setup.xlsx according to user's input
+    FSV_file_write = load_workbook(filename = "Test_Setup.xlsx") # load an existing .xlsx file
+    sheet = FSV_file_write["ACP"] # load existing sheet named "ACP"
+    sheet.cell(row = 1, column = 2, value = Test_frequency) # write test frequency in this sheet
+    FSV_file_write.save("Test_Setup.xlsx") # save existing .xlsx file
+# above codes are for setting test frequency in Test_Setup.xlsx according to user's input
+
+
+    Centre_frequency = sheet["B1"].value # get all parameters from Test_Setup.xlsx
+    Span_frequency = sheet["B2"].value #
+    RBW = sheet["B3"].value #
+    VBW = sheet["B4"].value #
+    RF_level = sheet["B5"].value #
+    Attenuation = sheet["B6"].value #
+    RefLev_offset = sheet["B7"].value#
+    Trace_RMS = sheet["B8"].value #
 
     Tx_CHBW = sheet["B10"].value
     AJ_CHBW = sheet["B11"].value
@@ -33,7 +40,7 @@ def Tx_Adjacent_channel_power():
     FSV.write("SYST:DISP:UPD ON")
     FSV.write("CALC:MARK:FUNC:POW:SEL ACP")
 
-    FSV.write(f"FREQ:CENT {Centre_frequency}MHz")
+    FSV.write(f"FREQ:CENT {Centre_frequency}MHz") # set all parameters
     FSV.write(f"FREQ:SPAN {Span_frequency}kHz")
     FSV.write(f"BAND {RBW}Hz")
     FSV.write(f"BAND:VID {VBW}Hz")
