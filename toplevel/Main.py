9bf338ba7139bbaebeeb647482e9889815f2bacc
Aaron Fan <afan@gme.net.au>
2019-05-13 12:11:31 +1000

    combine frequency error and carrier power tests

--- toplevel/Main.py
+++ toplevel/Main.py
@@ -1,6 +1,5 @@
-import Modules
 from sys import exit
-import visa
+import Modules
 header = """
 GME Radio compliance test suite Version 1.0
 """
@@ -9,54 +8,39 @@ print("--------------------------------------------")
 print(header)
 print("--------------------------------------------")
 
-rm = visa.ResourceManager()
-SML = rm.open_resource('ASRL4::INSTR') # Unwanted Signal
-SMB = rm.open_resource('USB0::0x0AAD::0x0054::106409::INSTR') # Wanted Signal
-FSV = rm.open_resource('TCPIP0::192.168.10.9::hislip0::INSTR') # Spec An
-CMS = rm.open_resource('GPIB0::24::INSTR') # Audio Analyzer
-SML.clear()  # Clear instrument io buffers and status
-SMB.clear()
-FSV.clear()
-CMS.clear()
+# Test_Setup.xlsx has to be in the same folder where Main.py locate
 
-def Hellowworld():
-    idn_response1 = SML.query('*IDN?')  # Query the Identification string
-    idn_response2 = SMB.query('*IDN?')
-    idn_response3 = FSV.query('*IDN?')
-    idn_response4 = CMS.query('*IDN?')
-    print (f"Hello, I am {idn_response1}")
-    print (f"Hello, I am {idn_response2}")
-    print (f"Hello, I am {idn_response3}")
-    print (f"Hello, I am {idn_response4}")
+def Helloworld():
+    print("Blank")
     start()
 
 def ANZ4365():
-    print("1. Tx-Frequency error")
-    print("2. Tx-Carrier power")
-    print("3. Tx-Max deviation")
-    print("4. Tx-Adjacent channel power")
-    print("5. Tx-Transient performance")
-    print("6. Tx-Out of band modulation response")
-    print("7. Tx-Conducted spurious emissions ")
-    print("8. Rx-Spurious emissions")
+    print("--------------------------------------------")
+    print("ASNZS4365:2011")
+    print("--------------------------------------------")
+    print("1. Tx-Frequency error_Carrier power")
+    print("2. Tx-Max deviation")
+    print("3. Tx-Adjacent channel power")
+    print("4. Tx-Transient performance")
+    print("5. Tx-Out of band modulation response")
+    print("6. Tx-Conducted spurious emissions ")
+    print("7. Rx-Spurious emissions")
     print("0. Go back to last menu")
     while True:
         choice = input("> ")
         if choice == "1":
-            Modules.Tx_Frequency_error()
+            Modules.Tx_Frequency_error_Carrier_power()
         elif choice == "2":
-            Modules.Tx_Carrier_power()
-        elif choice == "3":
             Modules.Tx_Max_deviation()
-        elif choice == "4":
+        elif choice == "3":
             Modules.Tx_Adjacent_channel_power()
-        elif choice == "5":
+        elif choice == "4":
             Modules.Tx_Transient_performance()
-        elif choice == "6":
+        elif choice == "5":
             Modules.Tx_Out_of_band_modulation_response()
-        elif choice == "7":
+        elif choice == "6":
             Modules.Tx_Conducted_spurious_emissions()
-        elif choice == "8":
+        elif choice == "7":
             Modules.Rx_Spurious_emissions()
         elif choice == "0":
             start()
@@ -64,32 +48,32 @@ def ANZ4365():
             print("please enter number within the range from 0 to 8.")
 
 def ANZ4295():
+    print("--------------------------------------------")
+    print("ASNZS4295:2015")
+    print("--------------------------------------------")
     print("1. Tx-Frequency error")
-    print("2. Tx-Carrier power")
-    print("3. Tx-Max deviation")
-    print("4. Tx-Adjacent channel power")
-    print("5. Tx-Conducted spurious emissions")
-    print("6. Rx-Adjacent channel selectivity")
-    print("7. Rx-Spurious response immunity ")
-    print("8. Rx-Blocking immunity")
+    print("2. Tx-Max deviation")
+    print("3. Tx-Adjacent channel power")
+    print("4. Tx-Conducted spurious emissions")
+    print("5. Rx-Adjacent channel selectivity")
+    print("6. Rx-Spurious response immunity ")
+    print("7. Rx-Blocking immunity")
     print("0. Go back to last menu")
     while True:
         choice = input("> ")
         if choice == "1":
-            Modules.Tx_Frequency_error()
+            Modules.Tx_Frequency_error_Carrier_power()
         elif choice == "2":
-            Modules.Tx_Carrier_power()
-        elif choice == "3":
             Modules.Tx_Max_deviation()
-        elif choice == "4":
+        elif choice == "3":
             Modules.Tx_Adjacent_channel_power()
-        elif choice == "5":
+        elif choice == "4":
             Modules.Tx_Conducted_spurious_emissions()
-        elif choice == "6":
+        elif choice == "5":
             Modules.Rx_Adjacent_channel_selectivity()
-        elif choice == "7":
+        elif choice == "6":
             Modules.Rx_Spurious_response_immunity()
-        elif choice == "8":
+        elif choice == "7":
             Modules.Rx_Blocking_immunity()
         elif choice == "0":
             start()
@@ -112,6 +96,6 @@ def start():
         elif choice == "4":
             exit(0)
         else:
-            print("please enter number 1, 2, or 3.")
+            print("please enter number 1, 2, 3 or 4.")
 
 start()
