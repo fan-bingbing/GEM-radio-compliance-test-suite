d7c63a4e01107d950e4683a97739c32895d1f800
Aaron Fan <afan@gme.net.au>
2019-05-16 09:41:58 +1000

    out of band modulation response completed

--- toplevel/Modules.py
+++ toplevel/Modules.py
@@ -1,4 +1,5 @@
 import sys
+import math
 # below codes are for importing files from different directory
 # a __init__.py file has to be exist in both "import from" and "import to" folder
 sys.path.insert(0, r'C:\Users\afan\documents\commercial-radio-test-suite\midlevel')
@@ -10,6 +11,7 @@ import FSV_screenshot as Scr
 import RadioControl as Rcon
 import FreqError_Power as FEP
 import Max_Deviation as MAD
+import OOB_ModRes as OOB
 import ACP
 
 
@@ -36,7 +38,9 @@ def Tx_Max_deviation():
 
 def Tx_Adjacent_channel_power():
     Test_frequency = input("Input test frequency in Mhz or press CTRL+C to quit > ")
+    Rcon.RadioControl('com7', 'ON')
     print(ACP.Tx_Adjacent_channel_power(Test_frequency))
+    Rcon.RadioControl('com7', 'OFF')
     file_name = input("To save the screenshot, input the filename (***.bmp) or press CTRL+C to quit > ")
     Scr.Screenshot(file_name)
     exit(0)
@@ -46,7 +50,17 @@ def Tx_Transient_performance():
     exit(0)
 
 def Tx_Out_of_band_modulation_response():
-    print("blank")
+    Test_frequency = input("Input test frequency in Mhz or press CTRL+C to quit > ")
+    Rcon.RadioControl('com7', 'ON')
+    Abs_deviation = OOB.OOB_ModRes(Test_frequency)
+    Rel_deviation = []
+    for i in range(0,8):
+        Rel_deviation.append(20*math.log10(Abs_deviation[i]/Abs_deviation[8]))
+    print(f"Out of Band Modulation Response test {Abs_deviation[9]}")
+    print(f"Absolution deviation:{Abs_deviation}")
+    print(f"Relative deviation:{Rel_deviation}")
+    Rcon.RadioControl('com7', 'OFF')
+
     exit(0)
 
 def Tx_Conducted_spurious_emissions():
