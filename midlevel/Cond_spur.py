5446ec2a43ef13b4249cfdb899d79aafd9884cbe
Aaron Fan <afan@gme.net.au>
2019-05-16 12:08:44 +1000

    add a global variable to select limit line automatically

--- midlevel/Cond_spur.py
+++ midlevel/Cond_spur.py
@@ -1,8 +1,33 @@
+import visa
+import time
 from openpyxl import load_workbook
+import config # contains global variables
 
-def Tx_Conducted_spurious_emissions_setup(setup_sheet):
-    FSV_file_write = load_workbook(filename = "FSV_Setup.xlsx") # create a workbook from existing .xlsx file
+def Conducted_spurious(setup_sheet):
+    rm = visa.ResourceManager()
+    FSV = rm.open_resource('TCPIP0::192.168.10.9::hislip0::INSTR') # Spec An
+
+    # below codes are for selecting correct limit line based on global varable limit_line_factor
+    if config.limit_line_factor == 2:
+        FSV_file_write = load_workbook(filename = "Test_Setup.xlsx") # load an existing .xlsx file
+        sheet = FSV_file_write[setup_sheet] # load existing sheet named "ACP"
+        sheet.cell(row = 16, column = 2, value = "ON") # write test frequency in this sheet
+        sheet.cell(row = 18, column = 2, value = "OFF") # write test frequency in this sheet
+        FSV_file_write.save("Test_Setup.xlsx") # save existing .xlsx file
+    else:
+        FSV_file_write = load_workbook(filename = "Test_Setup.xlsx") # load an existing .xlsx file
+        sheet = FSV_file_write[setup_sheet] # load existing sheet named "ACP"
+        sheet.cell(row = 16, column = 2, value = "OFF") # write test frequency in this sheet
+        sheet.cell(row = 18, column = 2, value = "ON") # write test frequency in this sheet
+        FSV_file_write.save("Test_Setup.xlsx") # save existing .xlsx file
+
+    # above codes are for selecting correct limit line based on global varable limit_line_factor
+
+    FSV.clear()
+
+    FSV_file_write = load_workbook(filename = "Test_Setup.xlsx") # create a workbook from existing .xlsx file
     sheet = FSV_file_write[setup_sheet] # load setup sheet in .xlsx to sheet
+
     start_frequency = sheet["B1"].value # get start frequency value from sheet
     stop_frequency = sheet["B2"].value # get stop frequency value from sheet
     RBW = sheet["B3"].value # get RBW value from sheet
@@ -17,8 +42,11 @@ def Tx_Conducted_spurious_emissions_setup(setup_sheet):
     Trans2_ON = sheet["B12"].value
     Transducer3 = sheet["B13"].value # High pass filter
     Trans3_ON = sheet["B14"].value
-    Limit_line = sheet["B15"].value # ASNZS4365:2011
-    Sweep_points= sheet["B16"].value # get sweep points
+    Limit_line_1 = sheet["B15"].value # ASNZS4365:2011
+    Limit_line_1_ON = sheet["B16"].value # ASNZS4365:2011
+    Limit_line_2 = sheet["B17"].value # ASNZS4365:2011
+    Limit_line_2_ON = sheet["B18"].value # ASNZS4365:2011
+    Sweep_points= sheet["B19"].value # get sweep points
 
 
     FSV.write(f"*RST")
@@ -38,10 +66,21 @@ def Tx_Conducted_spurious_emissions_setup(setup_sheet):
     FSV.write(f"CORR:TRAN {Trans2_ON}")
     FSV.write(f"CORR:TRAN:SEL '{Transducer3}'")
     FSV.write(f"CORR:TRAN {Trans3_ON}")
-    FSV.write(f"CALC:LIM:NAME '{Limit_line}'")
-    FSV.write(f"CALC:LIM:UPP:STAT ON")
-    FSV.write(f"SWE:POIN 3000")
+    FSV.write(f"CALC:LIM:NAME '{Limit_line_1}'")
+    FSV.write(f"CALC:LIM:UPP:STAT {Limit_line_1_ON}")
+    FSV.write(f"CALC:LIM:NAME '{Limit_line_2}'")
+    FSV.write(f"CALC:LIM:UPP:STAT {Limit_line_2_ON}")
+    FSV.write(f"SWE:POIN {Sweep_points}")
     FSV.write(f"DISP:TRAC:MODE MAXH")
+    time.sleep(5)# wait 10 seconds for trace to stable
+    FSV.write(f"DISP:TRAC:MODE VIEW")
+    FSV.query("*OPC?")
 
-def Tx_Conducted_spurious_emissions_test():
-    print("blank")
+    FSV.write("CALC:MARK1:MAX")
+    Frequency = float(FSV.query("CALC:MARK1:X?"))
+    Level = float(FSV.query("CALC:MARK1:Y?"))
+
+    indication = (FSV.query("*OPC?")).replace("1","Completed") # replace return character "1" to "completed"
+    FSV.close()
+
+    return {'Mark frequency':Frequency, 'Mark level':Level, 'Indication':indication}
