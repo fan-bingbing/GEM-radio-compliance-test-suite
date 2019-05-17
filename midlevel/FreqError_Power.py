2628c02741e667846ddf45e03ffd31e32287de54
Aaron Fan <afan@gme.net.au>
2019-05-17 16:36:36 +1000

    add data logging to result excel

--- midlevel/FreqError_Power.py
+++ midlevel/FreqError_Power.py
@@ -2,39 +2,43 @@ import visa
 import time
 from openpyxl import load_workbook
 
-
+# naming rules for excel:
+# 1. for operating variables related to Test_Setup.xlsx: use SFile_write, SSheet
+# 2. for operating variables related to Test_Result.xlsx: use RFile_write, RSheet
 
 def FreqError_Power(Test_frequency):
     rm = visa.ResourceManager()
     FSV = rm.open_resource('TCPIP0::192.168.10.9::hislip0::INSTR') # Spec An
     # below codes are for setting test frequency in Test_Setup.xlsx according to user's input
-    FSV_file_write = load_workbook(filename = "Test_Setup.xlsx") # load an existing .xlsx file
-    sheet = FSV_file_write["Freq_Error_Power"] # load existing sheet named "ACP"
-    sheet.cell(row = 1, column = 2, value = Test_frequency) # write test frequency in this sheet
-    FSV_file_write.save("Test_Setup.xlsx") # save existing .xlsx file
+    SFile_write = load_workbook(filename = "Test_Setup.xlsx") # load an existing .xlsx file
+    SSheet = SFile_write["Freq_Error_Power"] # load existing SSheet named "ACP"
+    SSheet.cell(row = 1, column = 2, value = Test_frequency) # write test frequency in this SSheet
+    SFile_write.save("Test_Setup.xlsx") # save existing .xlsx file
     # above codes are for setting test frequency in Test_Setup.xlsx according to user's input
 
-    FSV_file_write = load_workbook(filename = "Test_Setup.xlsx") # create a workbook from existing .xlsx file
-    sheet = FSV_file_write["Freq_Error_Power"] # load setup sheet in .xlsx to sheet
-    Centre_frequency = sheet["B1"].value #
-    Span_frequency = sheet["B2"].value #
-    RBW = sheet["B3"].value #
-    VBW = sheet["B4"].value #
-    RF_level = sheet["B5"].value #
-    Attenuation = sheet["B6"].value #
-    RefLev_offset = sheet["B7"].value#
-    Trace_peak = sheet["B8"].value #
-    Transducer1 = sheet["B9"].value # cable
-    Trans1_ON = sheet["B10"].value
-    Transducer2 = sheet["B11"].value # 30dB attenuator
-    Trans2_ON = sheet["B12"].value
-    Transducer3 = sheet["B13"].value # High pass filter
-    Trans3_ON = sheet["B14"].value
-    Limit_line_1 = sheet["B15"].value # ASNZS4365:2011
-    Limit_line_1_ON = sheet["B16"].value # ASNZS4365:2011
-    Limit_line_2 = sheet["B17"].value # ASNZS4295:2015
-    Limit_line_2_ON = sheet["B18"].value # ASNZS4295:2015
-    Sweep_points= sheet["B19"].value # get sweep points
+    RFile_write = load_workbook(filename = "Test_Result.xlsx") # load Test_Result.xlsx
+    RSheet = RFile_write["Ferror_Pow"] # load "Ferror_Pow" sheet in .xlsx
+
+
+    Centre_frequency = SSheet["B1"].value #
+    Span_frequency = SSheet["B2"].value #
+    RBW = SSheet["B3"].value #
+    VBW = SSheet["B4"].value #
+    RF_level = SSheet["B5"].value #
+    Attenuation = SSheet["B6"].value #
+    RefLev_offset = SSheet["B7"].value#
+    Trace_peak = SSheet["B8"].value #
+    Transducer1 = SSheet["B9"].value # cable
+    Trans1_ON = SSheet["B10"].value
+    Transducer2 = SSheet["B11"].value # 30dB attenuator
+    Trans2_ON = SSheet["B12"].value
+    Transducer3 = SSheet["B13"].value # High pass filter
+    Trans3_ON = SSheet["B14"].value
+    Limit_line_1 = SSheet["B15"].value # ASNZS4365:2011
+    Limit_line_1_ON = SSheet["B16"].value # ASNZS4365:2011
+    Limit_line_2 = SSheet["B17"].value # ASNZS4295:2015
+    Limit_line_2_ON = SSheet["B18"].value # ASNZS4295:2015
+    Sweep_points= SSheet["B19"].value # get sweep points
 
     FSV.write(f"*RST")
     FSV.write("SYST:DISP:UPD ON")
@@ -65,9 +69,12 @@ def FreqError_Power(Test_frequency):
 
     FSV.write("CALC:MARK1:MAX")
     Frequency = FSV.query("CALC:MARK1:X?")
-    Level = FSV.query("CALC:MARK1:Y?")
+    Level = float(FSV.query("CALC:MARK1:Y?"))
     Frequency_error = float(Frequency) - float(Centre_frequency)*1e6
     indication = (FSV.query("*OPC?")).replace("1","Frequency error test Completed") # replace return character "1" to "completed"
+    RSheet.cell(row = 2, column = 1, value = Frequency_error)
+    RSheet.cell(row = 2, column = 2, value = Level)
+    RFile_write.save("Test_Result.xlsx") # save existing .xlsx file
     FSV.close()
 
     return {'Frequency_error':Frequency_error, 'Carrier_power':Level, 'Indication':indication}
