808abf61af5afc13323c599713d3352f23a16684
yu <fanyu1980@hotmail.com>
2019-06-03 10:54:32 +1000

    add init file

--- bottomlevel/send_email_FSP.py
+++ bottomlevel/send_email_FSP.py
@@ -1,23 +1,25 @@
 import smtplib
 from email.message import EmailMessage
 
-msg = EmailMessage()
 
-with open('C:\\Users\\YU FAN\\Desktop\\pynotes.txt') as fp1:
-    msg.set_content(fp1.read())
+def SendEmail(Subject, From, To, Timestamp):
+    msg = EmailMessage()
+
+    #with open('C:\\Users\\Aaron Fan\\Documents\\GEM-radio-compliance-test-suite\\bottomlevel\\EmailContent.txt') as fp1:
+    msg.set_content(f"Test completed at {Timestamp}")
 
 
-with open('C:\\Users\\YU FAN\\Desktop\\Book1.xlsx', 'rb') as fp2:
-    content = fp2.read()
-    msg.add_attachment(content, maintype='application', subtype='xlsx', filename='Book1.xlsx')
+    with open('C:\\Users\\Aaron Fan\\Documents\\GEM-radio-compliance-test-suite\\scripts\\CM60_Result.xlsx', 'rb') as fp2:
+        content = fp2.read()
+        msg.add_attachment(content, maintype='application', subtype='xlsx', filename='CM60_Result.xlsx')
 
-msg['subject'] = 'hehe'
-msg['From'] = 'fanyu1980@hotmail.com'
-msg['To'] = 'fansunsun@163.com'
-#msg['Cc']
+    msg['subject'] = Subject
+    msg['From'] = From
+    msg['To'] = To
+    #msg['Cc']
 
-s = smtplib.SMTP('smtp.live.com', 587)# SMTP server used for sending
-s.starttls()# puts connection to SMTP server in TLS mode
-s.login("fanyu1980@hotmail.com", "Password782578")
-s.send_message(msg)
-s.quit()
+    s = smtplib.SMTP('smtp.live.com', 587)# SMTP server used for sending
+    s.starttls()# puts connection to SMTP server in TLS mode
+    s.login("fanyu1980@hotmail.com", "Password782578")
+    s.send_message(msg)
+    s.quit()
