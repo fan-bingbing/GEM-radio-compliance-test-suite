be0b65f5801cf0be7e9fff1592cc74d12e460803
fansunsun <fanyu1980@hotmail.com>
2019-07-01 14:14:58 +1000

    add soundcard class

--- toplevel/Modules.py
+++ toplevel/Modules.py
@@ -1,3 +1,8 @@
+"""
+GME radio compliance test suite scripts for class and function definition .
+Copyright (C) 2019 Standard Communications Pty Ltd (GME). All rights reserved.
+"""
+
 import sys
 import os
 import math
@@ -8,8 +13,12 @@ import serial
 import re
 import config
 import datetime
-from decimal import *
-#print(decimal.__file__)
+from decimal import * # should aviod wildcard import mentioned in PEP08
+import sounddevice as sd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.fftpack import fft
+
 
 
 # below codes are for importing files from different directory
@@ -20,14 +29,83 @@ sys.path.insert(0, r'C:\Users\afan\kallithea\aaron test\toplevel')
 # above codes are for importing files from different directory
 # a __init__.py file has to be exist in both "import from" and "import to" folder
 
+class SoundCard(object):
+    def __init__(self):
+        self.gain = 10
+        self.duration = 0.1
+        self.sample_rate = 44100
+        self.device = ('Microphone (Sound Blaster Play!, MME',
+                       'Speakers (Sound Blaster Play! 3, MME')
+        sd.query_devices(self.device[0])
+        sd.default.device = self.device
+        sd.default.channels = 1
+        sd.default.samplerate = self.sample_rate
+
+    def get_sample(self):
+        x1 = sd.rec(int(self.duration*self.sample_rate), dtype='float32', blocking=True) # record 0.1 seconds input signal from microphone
+        x2 = self.gain*x1[int(self.duration*self.sample_rate/1000):int(self.duration*self.sample_rate)] # take effective samples
+        x3 = x2[0:int(80*(self.sample_rate/1000))] # take first 80ms duration samples
+        x4 = x3.flatten()# transfer 2D array to 1D, ready for fft operation, fft function can only take 1D array as input
+        t = 80*np.linspace(0, 1, np.size(x4))# generate 80ms time axis VS samples in first 80ms
+
+        X = fft(x4)
+        n = np.size(t)
+        fr = (self.sample_rate/2)*np.linspace(0, 1, int(n/2))
+        X_m = (2/n)*abs(X[0:np.size(fr)])
+        return {'Time_level':x4, 'Time':t, 'Freq_level':X_m, 'Freq':fr}
+
+    def get_SINAD(self, dict):
+
+        X_mmax = np.max(dict['Freq_level']) # find maximum value in numpy array
+        fr_index = np.where(dict['Freq_level'] == X_mmax) # locate index in numpy array
+        fr_max = dict['Freq'][fr_index] # find corresponding frequency of maximum value
+
+        mask = np.full(np.size(dict['Freq']), 1) # initialize a mask numpy array with size=fr, value=1
+
+        mask[fr_index[0]]=0 # make notch filter mask by assigning some points=0 where certain bandwidth of carrier was covered
+        for i in range(1,45): # notch filter cover 40 bins
+            mask[fr_index[0]-i]=0
+            mask[fr_index[0]+i]=0
+
+        SND = np.sum(dict['Freq_level']) # add all points value to get SND
+        ND = np.sum(dict['Freq_level'] * mask) # apply notch filter, get ND
+        SINAD = 20 * np.log10(SND/ND) # calculate SINAD
+        return SINAD
+
+
+    def live_plots(self):
+        dict = self.get_sample()
+        fig, (ax0, ax1) = plt.subplots(nrows=2)
+        ax0.set_title('Sinusoidal Signal')
+        ax0.set_xlabel('Time(ms)')
+        ax0.set_ylabel('Amplitude')
+        ax0.set_ylim(-2, 2)
+        line0, = ax0.plot(dict['Time'], dict['Time_level'])
+
+        ax1.set_title('Magnitude Spectrum')
+        ax1.set_xlabel('Frequency(Hz)')
+        ax1.set_ylabel('Magnitude')
+        ax1.set_ylim(0, 1.5)
+        ax3 = ax1.text(0.3, 0.9, 'SINAD', transform=ax1.transAxes)
+        line1, = ax1.plot(dict['Freq'], dict['Freq_level'])
+
+        fig.subplots_adjust(hspace=0.6)
+
+        while True:
+            dict = self.get_sample()
+            line0.set_ydata(dict['Time_level']) #update data in plots only in loop instead of update entire plots
+            line1.set_ydata(dict['Freq_level'])
+            ax3.set_text('SINAD = %.2f dB' % self.get_SINAD(dict=dict)) # update SINAD value
+
+            plt.pause(0.01)# this command will call plt.show()
 
 class SpecAn(object):
 
-    def __init__(self):
-
+    def __init__(self, address):
         self.rm = visa.ResourceManager()
-        self.SP = self.rm.open_resource('TCPIP0::192.168.10.9::hislip0::INSTR') # Spec An
+        self.SP = self.rm.open_resource(address) # Spec An
         self.book = load_workbook(filename = "Test_Setup.xlsx") # load an existing .xlsx file
+
 
     def FEP_Setup(self, freq):
 
@@ -343,6 +421,20 @@ class SigGen(object):
         self.SG.write(f":FM:INT:FREQ {self.Frequency_AF}kHz")
         self.SG.write(f":OUTP2:VOLT {self.Level_AF}mV")
         self.SG.write(f":OUTP2 {self.AF_output_on}")
+
+    def Tx_Setup(self):
+        self.sheet = self.book["ACP"]
+        # following code is to initialize SML
+        self.Frequency_AF = self.sheet["G1"].value #
+        self.Level_AF = self.sheet["G2"].value #
+        self.AF_output_on = self.sheet["G3"].value #
+
+        self.SG.write(f"*RST")
+        self.SG.write("SYST:DISP:UPD ON")
+        self.SG.write(f":FM:INT:FREQ {self.Frequency_AF}kHz")
+        self.SG.write(f":OUTP2:VOLT {self.Level_AF}mV")
+        self.SG.write(f":OUTP2 {self.AF_output_on}")
+
 
     def TranP_Setup(self, freq):
         self.sheet = self.book["Tran_Perform"]
@@ -896,11 +988,11 @@ def Rx_Spurious_response_immunity(freq, delta):
 
 def CHSW_SR():
 
-    Start_F = Decimal(451)/Decimal(1) # Decimal module make sure float numbers addition yeilds correct value
+    Start_F = Decimal(479.9875)/Decimal(1) # Decimal module make sure float numbers addition yeilds correct value
     CP50_Result.get_sheet("day2")
 
 
-    for i in range(0, 5):
+    for i in range(9, 11):
         Spur_Res = Rx_Spurious_response_immunity(freq=Start_F, delta=Decimal(-2*38.85))
         CP50_Result.write(row = i+2, column = 1, value = Start_F)
         CP50_Result.write(row = i+2, column = 2, value = Start_F+Decimal(-2*38.85))
@@ -914,11 +1006,44 @@ def CHSW_SR():
 
 
 
-#FSV = SpecAn()
-CP50 = Radio("com7")
-SML = SigGen('ASRL4::INSTR')
-SMB = SigGen('USB0::0x0AAD::0x0054::106409::INSTR')
-CMS = SigGen('GPIB0::24::INSTR')
+try:
+    SC = SoundCard()
+except BaseException:
+    print("FSV is not on.")
+    pass
+
+SC.live_plots()
+
+try:
+    FSV = SpecAn('TCPIP0::192.168.10.9::hislip0::INSTR')
+except BaseException:
+    print("FSV is not on.")
+    pass
+
+try:
+    CP50 = Radio('com7')
+except BaseException:
+    pass
+
+try:
+    SML = SigGen('ASRL4::INSTR')
+except BaseException:
+    print("SML is not on.")
+    pass
+
+try:
+    SMB = SigGen('USB0::0x0AAD::0x0054::106409::INSTR')
+except BaseException:
+    print("SMB is not on.")
+    pass
+
+try:
+    CMS = SigGen('GPIB0::24::INSTR')
+except BaseException:
+    print("CMS is not on.")
+    pass
+
 Result_sheet = Excel("Test_Result.xlsx")
 CP50_Result = Excel("CP50_Result.xlsx")
+
 getcontext().prec = 10 # set 10 decimal values precision
